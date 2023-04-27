24a1fa216e18a89f62b7b7720ff1e55a06157361
Jean Marco <fzafrc@gmail.com>
2023-04-27 01:12:06 -0500

    editar y panel

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -14,6 +14,17 @@
 </head>
 
 <body>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Communes') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
     <div class="container">
         <h1>lista</h1>
         <a href="{{ route('cliente.create') }}" class="btn btn-success">Agregar</a>
@@ -51,8 +62,12 @@
                 @endforeach
             </tbody>
         </table>
+</x-app-layout>
     </div>
-
+    </div>
+    </div>
+    </div>
+    </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
