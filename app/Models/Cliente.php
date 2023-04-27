7b16247121855461acbcdd158ac7d97258c368ce
Jean Marco <fzafrc@gmail.com>
2023-04-26 19:26:03 -0500

    Modelo y controlador

---
+++ app/Models/Cliente.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    use HasFactory;
+    protected $table = 'tb_cliente';
+    protected $primarykey ='id_cliente';
+    protected $timestamp = false;
+
+}
